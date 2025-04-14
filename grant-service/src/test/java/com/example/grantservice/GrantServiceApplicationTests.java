3daf667dd1d2aa5a07e38f7a0cebffa706bae422
stinaosmani <stinaosmani@gmail.com>
2025-04-14 21:03:22 +0200

    Initial commit with grant-service and application-service

---
+++ grant-service/src/test/java/com/example/grantservice/GrantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
