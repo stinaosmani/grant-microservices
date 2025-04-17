68c9d80ee9b492005845816dce0510a314225915
stinaosmani <stinaosmani@gmail.com>
2025-04-17 19:53:56 +0200

    updated Application Service implementation

--- application-service/src/main/java/com/example/applicationservice/app/service/ApplicationServiceImpl.java
+++ application-service/src/main/java/com/example/applicationservice/app/service/ApplicationServiceImpl.java
@@ -5,7 +5,6 @@ import com.example.applicationservice.app.dto.GrantDto;
 import com.example.applicationservice.domain.port.ApplicationService;
 import com.example.applicationservice.infrastructure.mapper.GrantMapper;
 import com.example.applicationservice.domain.model.Grant;
-import feign.FeignException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,18 +20,10 @@ public class ApplicationServiceImpl implements ApplicationService {
 
     @Override
     public String applyForGrant(String grantId) {
-        try {
-            Grant grant = grantClient.getGrantById(grantId);
-            GrantDto dto = grantMapper.toDto(grant);
+        Grant grant = grantClient.getGrantById(grantId);
+        GrantDto dto = grantMapper.toDto(grant);
 
-            return "Application submitted for: " + dto.getTitle() +
-                    " (" + dto.getCategory() + "), Amount: €" + dto.getAmount();
-        } catch (FeignException.NotFound ex) {
-            return "Grant with ID " + grantId + " not found.";
-        } catch (FeignException ex) {
-            return "Error communicating with grant service.";
-        } catch (Exception ex) {
-            return "An unexpected error occurred while applying for the grant.";
-        }
+        return "Application submitted for: " + dto.getTitle() +
+                " (" + dto.getCategory() + "), Amount: €" + dto.getAmount();
     }
 }
